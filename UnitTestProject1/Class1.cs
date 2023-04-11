35fd89c746a735f6a33c10bc8538583848939b39
bogdanborzianytsia <128235605+bogdanborzianytsia@users.noreply.github.com>
2023-04-11 09:40:03 +0300

    Add files via upload

---
+++ UnitTestProject1/Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chainofduties;
+
+namespace Chainofduties
+{
+
+    public abstract class Handler 
+    {
+        protected Handler successor;
+        public void SetSuccessor(Handler successor)
+        {
+            this.successor = successor;
+        }
+        public abstract string HandleRequest(string quest);
+    }
+
+    public class ConcreteHandler1 : Handler
+    {
+        public override string HandleRequest(string quest)
+        {
+
+            if (quest == "Hi")
+            {
+                return "Hi, how can i help you?";
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+
+    public class ConcreteHandler2 : Handler
+    {
+        public override string HandleRequest(string quest)
+        {
+            if (quest == "Can you help me?")
+            {
+                return "Yes i can help";
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+
+    public class ConcreteHandler3 : Handler
+    {
+        public override string HandleRequest(string quest)
+        {
+            if (quest == "Can you plus numbers?")
+            {
+                return "Sure i can do it";
+
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+    public class ConcreteHandler4 : Handler
+    {
+        public override string HandleRequest(string quest)
+        {
+            if (quest == "how much will 8 add to 13?")
+            {
+                return "it will be 21";
+
+            }
+            else
+            {
+                return "Eror";
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Runtime.InteropServices;
+using Chainofduties;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string test = "Hi";
+            string excpected = "Hi, how can i help you?";
+            Handler h1 = new ConcreteHandler1();
+            string actual = h1.HandleRequest(test);
+            Assert.AreEqual(actual, excpected);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string test = "Can you help me?";
+            string excpected ="Yes i can help";
+            Handler h2 = new ConcreteHandler2();
+            string actual = h2.HandleRequest(test);
+            Assert.AreEqual(actual, excpected);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string test = "Can you plus numbers?";
+            string excpected = "Sure i can do it";
+            Handler h3 = new ConcreteHandler3();
+            string actual = h3.HandleRequest(test);
+            Assert.AreEqual(actual, excpected);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string test = "how much will 8 add to 13?";
+            string excpected = "it will be 21";
+            Handler h4 = new ConcreteHandler4();
+            string actual = h4.HandleRequest(test);
+            Assert.AreEqual(actual, excpected);
+        }
+    }
+}
